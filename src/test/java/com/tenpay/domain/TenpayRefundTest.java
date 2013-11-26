672eff09deee538cbbd6120c797e7fbf10f0c3f8
xiaokaceng <xiaokaceng@gmail.com>
2013-11-26 16:23:28 +0800

    修改退款单元测试证书文件路径

--- src/test/java/com/tenpay/domain/TenpayRefundTest.java
+++ src/test/java/com/tenpay/domain/TenpayRefundTest.java
@@ -9,7 +9,7 @@ public class TenpayRefundTest extends TenpayAbstractTest {
 	
 	@Test
 	public void test() throws Exception {
-		TenpayRefund refund = new TenpayRefund(PARTNER, KEY, REFUND_URL, "NT201310100000000013", "1900000109201310100370906423", "1900000109201310100370906423", 2, 2, PARTNER, "111111", "/tenpay/cacert.pem", "/tenpay/1900000109.pfx");
+		TenpayRefund refund = new TenpayRefund(PARTNER, KEY, REFUND_URL, "NT201310100000000013", "1900000109201310100370906423", "1900000109201310100370906423", 2, 2, PARTNER, "111111", "src/main/resources/tenpay/cacert.pem", "src/main/resources/tenpay/1900000109.pfx");
 		System.out.println(refund.execute().getContent());
 		Assert.assertEquals(REFUND_SUCCESS_CODE, refund.execute().getParameter("refund_status"));
 	}
